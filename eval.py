35d07340d965e5a4d851a4b5842bd818deb6630e
Lars Schiller (AmP) <lars@familysc.de>
2019-02-01 13:43:06 +0100

    guess geckos pose from measured data with respect to measurement uncertanty

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ from itertools import islice
 
 import load
 import save
-
+import kin_model
 
 SAVE = False
 
@@ -41,7 +41,7 @@ def remove_offset_time_xy(data):
     sc = 100/640.  # px->cm
     for idx in range(6):
         data['x{}'.format(idx)] = [(x-xstart)*sc for x in data['x{}'.format(idx)]]
-        data['y{}'.format(idx)] = [(y-ystart)*sc for y in data['y{}'.format(idx)]]
+        data['y{}'.format(idx)] = [-(y-ystart)*sc for y in data['y{}'.format(idx)]]
 
     return data
 
@@ -66,6 +66,10 @@ def rm_offset(lis):
     return [val-offset for val in lis]
 
 
+def add_offset(lis, offset):
+    return [val+offset for val in lis]
+
+
 def calc_mean_stddev(mat):
     mu1 = np.nanmean(mat, axis=1)
     sigma1 = np.nanstd(mat, axis=1)
@@ -87,10 +91,10 @@ def make_matrix(data, cycle_idx):
     return mat, min_idx
 
 
-def calc_centerpoint(data_set, cycles, axis='x'):
+def calc_centerpoint(data_set, cycles, axis='x', marks=range(6)):
     X = []
     min_dist = min([idx[-1] - idx[0] for idx in cycles])
-    markers = ['{}{}'.format(axis, idx) for idx in range(6)]
+    markers = ['{}{}'.format(axis, idx) for idx in marks]
 
     for exp in range(len(data_set)):
         start = cycles[exp][0]
@@ -105,8 +109,8 @@ def calc_centerpoint(data_set, cycles, axis='x'):
 
 
 def make_matrix_plain(data):
-    nSteps = len(data[0])
     nSets = len(data)
+    nSteps = min([len(data[idx]) for idx in range(nSets)])
     mat = np.ndarray((nSteps, nSets))
     for set_ in range(nSets):
         for step in range(nSteps):
@@ -134,6 +138,23 @@ def calc_foot_mean_of_all_exp(data_set, cycles):
     return X, Y, Xstd, Ystd
 
 
+def calc_mean_of_axis_in_exp_and_cycle(data_set, cycles, axis='x0'):
+    MU, SIG = [], []
+    min_len = min([len(cycle) for cycle in cycles])
+    cycles = [cycle[0:min_len] for cycle in cycles]  # cut all cycle to min len
+
+    for cycle_idx in range(min_len-1):
+        min_dist = min([cycles[exp][cycle_idx+1]-cycles[exp][cycle_idx] for exp in range(len(data_set))])
+        mu, sig = [], []
+        for idx in range(min_dist):
+            x_exp = [data_set[exp][axis][cycles[exp][cycle_idx]+idx] for exp in range(len(data_set))]
+            mu.append(np.nanmean(x_exp))
+            sig.append(np.nanstd(x_exp))
+        MU.append(mu)
+        SIG.append(sig)
+    return MU, SIG
+
+
 def load_data(exp_name, exp_idx=['00']):
     dset, Cycles = [], []
     for exp in exp_idx:
@@ -155,11 +176,11 @@ ________________________________________________
 # exp 0 data qualilty:
 
 #           0 1 2 3 4 5 6 7 8 9 10 11 12
-# small     1 0 1 1 1 1 1 1 1 1 x  x  x
+# small     1 0 1 1 1 1 x 1 1 1 x  x  x
 # big       1 1 0 0 1 1 1 1 1 0 0  1  1
 
 ggg = 1
-sets = ['{}'.format(idx).zfill(2) for idx in [0,2,3,4,5,6,7,8,9]]
+sets = ['{}'.format(idx).zfill(2) for idx in [0,2,3,4,5,7,8,9]]
 ds, cyc_small = load_data('small_0', sets)
 
 sets = ['{}'.format(idx).zfill(2) for idx in [0,1,4,5,6,7,8,11,12]]
@@ -171,178 +192,277 @@ color_ref = 'lightcoral'
 color_alp = 'red'
 
 
+if 0:
 
+    plt.figure()
+    # # small
+    centers, t = calc_centerpoint(ds, cyc_small)
+    mat = make_matrix_plain(centers)
+    mu, sigma = calc_mean_stddev(mat)
+    plt.plot(t, mu, '-', lw=2, label='p_{v0}', color=color_prs)
+    plt.fill_between(t, mu+sigma, mu-sigma, facecolor=color_prs, alpha=0.2)
 
-#plt.figure()
-#centers, t = calc_centerpoint(ds, cyc_small)
-#for row in centers:
-#    plt.plot(row)
+    # # big
+    centers, t = calc_centerpoint(db, cyc_big)
+    mat = make_matrix_plain(centers)
+    mu, sigma = calc_mean_stddev(mat)
+    plt.plot(t, mu, '-', lw=2, label='p_{v0}', color=color_alp)
+    plt.fill_between(t, mu+sigma, mu-sigma, facecolor=color_alp, alpha=0.2)
 
+    plt.xlabel('time (s)')
+    plt.ylabel('$\bar{x}$ (cm)')
+    plt.grid()
 
-plt.figure()
+    save.save_as_tikz('pics/Shift.tex')
 
-
-## small
-centers, t = calc_centerpoint(ds, cyc_small)
-mat = make_matrix_plain(centers)
-mu, sigma = calc_mean_stddev(mat)
-plt.plot(t, mu, '-', lw=2, label='p_{v0}', color=color_prs)
-plt.fill_between(t, mu+sigma, mu-sigma, facecolor=color_prs, alpha=0.2)
-
-
-## big
-centers, t = calc_centerpoint(db, cyc_big)
-mat = make_matrix_plain(centers)
-mu, sigma = calc_mean_stddev(mat)
-plt.plot(t, mu, '-', lw=2, label='p_{v0}', color=color_alp)
-plt.fill_between(t, mu+sigma, mu-sigma, facecolor=color_alp, alpha=0.2)
-
-plt.xlabel('time (s)')
-plt.ylabel('$\bar{x}$ (cm)')
-plt.grid()
-
-
-save.save_as_tikz('pics/Shift.tex')
-
-if SAVE:
-    plt.savefig('pics/Shift.png', dpi=500, facecolor='w', edgecolor='w',
-                orientation='portrait', papertype=None, format=None,
-                transparent=False, bbox_inches=None, pad_inches=0.1,
-                frameon=None, metadata=None)
-
-
-"""
-________________________________________________
-____________________Track___________
-________________________________________________
-"""
-
-
-
-plt.figure()
-plt.title('Track')
-
-col = ['red', 'orange', 'green', 'blue', 'magenta', 'darkred']
-X, Y, Xstd, Ystd = calc_foot_mean_of_all_exp(ds, cyc_small)
-for idx in range(6):
-    x = np.array(X[idx])
-    y = -np.array(Y[idx])
-    sigx = np.array(Xstd[idx])
-    sigy = np.array(Ystd[idx])
-    plt.plot(x, y, color=col[idx])
-    plt.fill_between(x, y+sigy, y-sigy, facecolor=col[idx], alpha=0.6)
-
-
-yshift = -50
-X, Y, Xstd, Ystd = calc_foot_mean_of_all_exp(db, cyc_big)
-for idx in range(6):
-    x = np.array(X[idx])
-    y = -(np.array(Y[idx]) + yshift)
-    sigx = np.array(Xstd[idx])
-    sigy = np.array(Ystd[idx])
-    plt.plot(x, y, color=col[idx])
-    plt.fill_between(x, y+sigy, y-sigy, facecolor=col[idx], alpha=0.5, label='mark_{}'.format(idx))
-
-
-
-
-######## plot gecko in first position:
-#exp = 0
-#cycle = 3
-#idx = cyc_big[exp][cycle]
-#positions = ([db[exp]['x{}'.format(foot)][idx] for foot in range(6)],
-#             [db[exp]['y{}'.format(foot)][idx] for foot in range(6)])
-#alpha = [db[exp]['aIMG{}'.format(foot)][idx] for foot in range(6)]
-#eps = db[exp]['eps'][idx]
-#
-#
-#alpha_ = alpha[0:3] + alpha[4:6]
-#pose, ell, bet = kinematic.extract_pose(alpha_, eps, positions)
-#
-#
-
-
-
-
-plt.axis('equal')
-plt.legend(loc='upper right')
-
-
-save.save_as_tikz('pics/Track.tex')
-
-if SAVE:
-    plt.savefig('pics/Track.png', dpi=500, facecolor='w', edgecolor='w',
-                orientation='portrait', papertype=None, format=None,
-                transparent=False, bbox_inches=None, pad_inches=0.1,
-                frameon=None, metadata=None)
+    if SAVE:
+        plt.savefig('pics/Shift.png', dpi=500, facecolor='w', edgecolor='w',
+                    orientation='portrait', papertype=None, format=None,
+                    transparent=False, bbox_inches=None, pad_inches=0.1,
+                    frameon=None, metadata=None)
 
 
 ###############################################################################
-########################### TRACK MEAN ########################################
+# ################## ALL CYCLE ANALYSE ########################################
 ###############################################################################
 
+def flat_list(l):
+    return [item for sublist in l for item in sublist]
 
 
-plt.figure()
-plt.title('Track Mean')
-smapleval = .8
+if 0:
+    plt.figure()
+    col = ['red', 'orange', 'green', 'blue', 'magenta', 'darkred']
+    yshift = -50
+    for axis in [4, 0, 1, 3, 2, 5]:
+        x, sigx = calc_mean_of_axis_in_exp_and_cycle(db, cyc_big, axis='x{}'.format(axis))
+        y, sigy = calc_mean_of_axis_in_exp_and_cycle(db, cyc_big, axis='y{}'.format(axis))
+        xxx = x
+        x = flat_list(x)
+        y = (np.array(flat_list(y)) + yshift)
+        sigy = np.array(flat_list(sigy))
+        plt.plot(x, y, color=col[axis])
+        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[axis], alpha=0.5)
+
+        # Small
+        x, sigx = calc_mean_of_axis_in_exp_and_cycle(ds, cyc_small, axis='x{}'.format(axis))
+        y, sigy = calc_mean_of_axis_in_exp_and_cycle(ds, cyc_small, axis='y{}'.format(axis))
+        x = flat_list(x)
+        y = (np.array(flat_list(y)))
+        sigy = np.array(flat_list(sigy))
+        plt.plot(x, y, color=col[axis])
+        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[axis], alpha=0.5)
 
 
-# Big
-yshift = -20
-X, _ = calc_centerpoint(db, cyc_big, axis='x')
-mat = make_matrix_plain(X)
-x, sigx = calc_mean_stddev(mat)
-
-Y, _ = calc_centerpoint(db, cyc_big, axis='y')
-mat = make_matrix_plain(Y)
-y, sigy = calc_mean_stddev(mat)
-y = -(y + yshift)
-
-x = np.array(downsample(x, smapleval))
-y = np.array(downsample(y, smapleval))
-sigy = np.array(downsample(sigy, smapleval))
-
-plt.plot(x, y, color=col[idx])
-plt.fill_between(x, y+sigy, y-sigy, facecolor=color_alp, alpha=0.5, label='mean_big')
-
-# Mean Small
-yshift = 0
-X, _ = calc_centerpoint(ds, cyc_small, axis='x')
-mat = make_matrix_plain(X)
-x, sigx = calc_mean_stddev(mat)
-
-Y, _ = calc_centerpoint(ds, cyc_small, axis='y')
-mat = make_matrix_plain(Y)
-y, sigy = calc_mean_stddev(mat)
-y = -(y + yshift)
-
-
-x = np.array(downsample(x, smapleval))
-y = np.array(downsample(y, smapleval))
-sigy = np.array(downsample(sigy, smapleval))
-
-plt.plot(x, y, color=col[idx])
-plt.fill_between(x, y+sigy, y-sigy, facecolor=color_prs, alpha=0.5, label='mean_small')
-
-plt.axis('equal')
-plt.xlabel('x position (cm)')
-plt.ylabel('y position (cm)')
-plt.grid()
-
-
-
-save.save_as_tikz('pics/Track_Mean.tex')
-
-if SAVE:
-    save.save_as_tikz('pics/Track_Mean.png')
-    plt.savefig('pics/Track_Mean.png', dpi=500, facecolor='w', edgecolor='w',
-                orientation='portrait', papertype=None, format=None,
-                transparent=False, bbox_inches=None, pad_inches=0.1,
-                frameon=None, metadata=None)
-
-
-
+    plt.xlabel('x (cm)')
+    plt.ylabel('y (cm)')
+    plt.axis('equal')
 
 
 plt.show()
+
+
+###############################################################################
+# ################## SINGLE CYCLE ANALYSE #####################################
+###############################################################################
+
+
+def calc_mean_of_axis_for_all_exp_and_cycles(data, cyc, axis, skipfirstlast=(0, 0)):
+    x, sigx = calc_mean_of_axis_in_exp_and_cycle(data, cyc, axis)
+    X = []
+    x0 = x[skipfirstlast[0]][0]
+    for x_ in x:
+        x_ = add_offset(rm_offset(x_), x0)
+        X.append(x_)
+    X = X[skipfirstlast[0]:-skipfirstlast[1]] if skipfirstlast[1]!=0 else X[skipfirstlast[0]:]
+    mat = make_matrix_plain(X)
+    xx, sigxx = calc_mean_stddev(mat)
+    return xx, sigxx
+
+
+if 1:
+    plt.figure()
+    col = ['red', 'orange', 'green', 'blue', 'magenta', 'darkred']
+    yshift = -50
+    skip_first = 1
+    skip_last = 1
+
+    positions = [{}, {}]
+    alpha = {}
+
+    for axis in [0, 1, 2, 3, 4, 5]:
+        x, sigx = calc_mean_of_axis_for_all_exp_and_cycles(
+                db, cyc_big, 'x{}'.format(axis), (skip_first, skip_last))
+        y, sigy = calc_mean_of_axis_for_all_exp_and_cycles(
+                db, cyc_big, 'y{}'.format(axis), (skip_first, skip_last))
+        a, siga = calc_mean_of_axis_for_all_exp_and_cycles(
+                db, cyc_big, 'aIMG{}'.format(axis), (skip_first, skip_last))
+
+        plt.plot(x, y, color=col[axis])
+        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[axis], alpha=0.5)
+
+        alpha[axis] = a
+        positions[0][axis] = x
+        positions[1][axis] = y
+    eps, sige = calc_mean_of_axis_for_all_exp_and_cycles(
+            db, cyc_big, 'eps', (skip_first, skip_last))
+
+    # ####### plot gecko in first, mid and end position:
+    for jdx, idx in enumerate([0, len(eps)/2, len(eps)-1]):
+        pos = ([positions[0][axis][idx] for axis in range(6)],
+               [-positions[1][axis][idx] for axis in range(6)])  # mind minus
+        alp = [alpha[axis][idx] for axis in range(6)]
+        alp_ = alp[0:2] + [-alp[3]] + alp[4:6]
+        eps_ = 3   # cheat
+
+        pose, ell, alp__ = kin_model.extract_pose(alp_, eps_, pos)
+        pose = (pose[0], [-val for val in pose[1]])         # flip again
+        plt.plot(pose[0], pose[1], '.', color=col[jdx])
+        print 'idx: ', ell, [a_ - a__ for a_, a__ in zip(alp_, alp__)]
+
+#        # Small
+#        x, sigx = calc_mean_of_axis_in_exp_and_cycle(ds, cyc_small, axis='x{}'.format(axis))
+#        y, sigy = calc_mean_of_axis_in_exp_and_cycle(ds, cyc_small, axis='y{}'.format(axis))
+#        x = flat_list(x)
+#        y = (np.array(flat_list(y)))
+#        sigy = np.array(flat_list(sigy))
+#        plt.plot(x, y, color=col[axis])
+#        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[axis], alpha=0.5)
+
+
+
+
+
+
+
+    plt.xlabel('x (cm)')
+    plt.ylabel('y (cm)')
+    plt.axis('equal')
+    plt.grid()
+
+
+plt.show()
+
+
+"""
+________________________________________________
+____________________Track of feet___________
+________________________________________________
+"""
+
+if 0:
+    plt.figure()
+    plt.title('Track')
+
+    axes = range(6)
+
+    col = ['red', 'orange', 'green', 'blue', 'magenta', 'darkred']
+    X, Y, Xstd, Ystd = calc_foot_mean_of_all_exp(ds, cyc_small)
+    for idx in axes:
+        x = np.array(X[idx])
+        y = np.array(Y[idx])
+        sigx = np.array(Xstd[idx])
+        sigy = np.array(Ystd[idx])
+        plt.plot(x, y, color=col[idx])
+        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[idx], alpha=0.6)
+
+    yshift = -50
+    X, Y, Xstd, Ystd = calc_foot_mean_of_all_exp(db, cyc_big)
+    for idx in axes:
+        x = np.array(X[idx])
+        y = (np.array(Y[idx]) + yshift)
+        sigx = np.array(Xstd[idx])
+        sigy = np.array(Ystd[idx])
+        plt.plot(x, y, color=col[idx])
+        plt.fill_between(x, y+sigy, y-sigy, facecolor=col[idx], alpha=0.5, label='mark_{}'.format(idx))
+
+    ######## plot gecko in first position:
+    #exp = 0
+    #cycle = 3
+    #idx = cyc_big[exp][cycle]
+    #positions = ([db[exp]['x{}'.format(foot)][idx] for foot in range(6)],
+    #             [db[exp]['y{}'.format(foot)][idx] for foot in range(6)])
+    #alpha = [db[exp]['aIMG{}'.format(foot)][idx] for foot in range(6)]
+    #eps = db[exp]['eps'][idx]
+    #
+    #
+    #alpha_ = alpha[0:3] + alpha[4:6]
+    #pose, ell, bet = kinematic.extract_pose(alpha_, eps, positions)
+    #
+    #
+
+    plt.xlabel('x (cm)')
+    plt.ylabel('y (cm)')
+    plt.axis('equal')
+
+
+#    save.save_as_tikz('pics/Track.tex')
+
+    if SAVE:
+        plt.savefig('pics/Track.png', dpi=500, facecolor='w', edgecolor='w',
+                    orientation='portrait', papertype=None, format=None,
+                    transparent=False, bbox_inches=None, pad_inches=0.1,
+                    frameon=None, metadata=None)
+
+###############################################################################
+# ########################## TRACK MEAN #######################################
+###############################################################################
+
+if 0:
+    plt.figure()
+    plt.title('Track Mean')
+    smapleval = .8
+
+
+    # Big
+    yshift = -20
+    X, _ = calc_centerpoint(db, cyc_big, axis='x')
+    mat = make_matrix_plain(X)
+    x, sigx = calc_mean_stddev(mat)
+
+    Y, _ = calc_centerpoint(db, cyc_big, axis='y')
+    mat = make_matrix_plain(Y)
+    y, sigy = calc_mean_stddev(mat)
+    y = (y + yshift)
+
+    x = np.array(downsample(x, smapleval))
+    y = np.array(downsample(y, smapleval))
+    sigy = np.array(downsample(sigy, smapleval))
+
+    plt.plot(x, y, color=col[idx])
+    plt.fill_between(x, y+sigy, y-sigy, facecolor=color_alp, alpha=0.5, label='mean_big')
+
+    # Mean Small
+    yshift = 0
+    X, _ = calc_centerpoint(ds, cyc_small, axis='x')
+    mat = make_matrix_plain(X)
+    x, sigx = calc_mean_stddev(mat)
+
+    Y, _ = calc_centerpoint(ds, cyc_small, axis='y')
+    mat = make_matrix_plain(Y)
+    y, sigy = calc_mean_stddev(mat)
+    y = (y + yshift)
+
+    x = np.array(downsample(x, smapleval))
+    y = np.array(downsample(y, smapleval))
+    sigy = np.array(downsample(sigy, smapleval))
+
+    plt.plot(x, y, color=col[idx])
+    plt.fill_between(x, y+sigy, y-sigy, facecolor=color_prs, alpha=0.5, label='mean_small')
+
+    plt.axis('equal')
+    plt.xlabel('x position (cm)')
+    plt.ylabel('y position (cm)')
+    plt.grid()
+
+    save.save_as_tikz('pics/Track_Mean.tex')
+
+    if SAVE:
+        save.save_as_tikz('pics/Track_Mean.png')
+        plt.savefig('pics/Track_Mean.png', dpi=500, facecolor='w',
+                    edgecolor='w', orientation='portrait', papertype=None,
+                    format=None, transparent=False, bbox_inches=None,
+                    pad_inches=0.1, frameon=None, metadata=None)
+
+
+
---
+++ kin_model.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 01 12:32:16 2019
+
+@author: AmP
+"""
+import numpy as np
+from scipy.optimize import minimize
+
+len_leg = 13
+len_tor = 18
+ell_nom = [len_leg, len_leg, len_tor, len_leg, len_leg]
+
+
+n_foot = 4
+n_limbs = 5
+
+arc_res = 40    # resolution of arcs
+
+blow, bup = 0.8, 1.5
+max_alp_dif = 20
+
+
+def extract_pose(alpha, eps, fpos, len_leg=len_leg, len_tor=len_tor):
+
+    # unknown
+    ell0 = [len_leg, len_leg, len_tor, len_leg, len_leg]
+    alpha0 = alpha
+    x0 = ell0 + alpha
+    xpos, ypos = fpos[0], fpos[1]
+
+    w1, w2, w3 = 10, .1, 1
+
+    def objective(x):
+        ell, alp = x[0:n_limbs], x[n_limbs:]
+        xpos_est, ypos_est = _calc_coords2(x, alp, eps, (xpos[0], ypos[0]))
+        err = [np.linalg.norm([xpos[idx]-xpos_est[idx],
+                               ypos[idx]-ypos_est[idx]]) for idx in range(6)]
+        len_dif = [abs(ell_nom[idx] - ell[idx]) for idx in range(n_limbs)]
+        alp_dif = [abs(alpha0[idx] - alp[idx]) for idx in range(n_limbs)]
+        obj = w1*sum(err) + w2*sum(alp_dif) + w3*sum(len_dif)
+
+        return obj
+
+    bleg = (blow*len_leg, bup*len_leg)
+    btor = (blow*len_tor, bup*len_tor)
+    bbet = [(val-max_alp_dif, val+max_alp_dif) for val in alpha0]
+    bnds = (bleg, bleg, btor, bleg, bleg, bbet[0], bbet[1], bbet[2], bbet[3], bbet[4])
+    solution = minimize(objective, x0, method='SLSQP', bounds=bnds)
+    x = solution.x
+    ell, alp = x[0:n_limbs], x[n_limbs:]
+    xpos_est, ypos_est = _calc_coords2(x, alp, eps, (xpos[0], ypos[0]))
+
+    r = ([xpos[0]]+xpos[2:4]+[xpos[5]],
+         [ypos[0]]+ypos[2:4]+[ypos[5]])
+
+    x_ = list(alp) + list(ell) + [eps]
+    (xa, ya), (fpx, fpy), (nfpx, nfpy) = get_repr(x_, r, [1, 1, 1, 1])
+
+    return (xa, ya), ell, alp
+
+
+def _calc_coords2(x, alp, eps, (xf1, yf1)):
+    ell, _ = x[0:n_limbs], x[n_limbs:]
+    c1, c2, _, _ = _calc_phi(alp, eps)
+    R = [_calc_rad(ell[i], alp[i]) for i in range(5)]
+
+    # coords cp upper left leg
+    xr1 = xf1 + np.cos(np.deg2rad(c1))*R[0]
+    yr1 = yf1 - np.sin(np.deg2rad(c1))*R[0]
+    # coords upper torso
+    xom = xr1 - np.sin(np.deg2rad(90-c1-alp[0]))*R[0]
+    yom = yr1 + np.cos(np.deg2rad(90-c1-alp[0]))*R[0]
+    # coords cp R2
+    xr2 = xom + np.cos(np.deg2rad(c1+alp[0]))*R[1]
+    yr2 = yom - np.sin(np.deg2rad(c1+alp[0]))*R[1]
+    # coords F2
+    xf2 = xr2 + np.sin(np.deg2rad(alp[1] - (90-c1-alp[0])))*R[1]
+    yf2 = yr2 + np.cos(np.deg2rad(alp[1] - (90-c1-alp[0])))*R[1]
+
+    # coords cp torso
+    xrom = xom + np.cos(np.deg2rad(90-c1-alp[0]))*R[2]
+    yrom = yom + np.sin(np.deg2rad(90-c1-alp[0]))*R[2]
+    # coords lower torso
+    xum = xrom - np.cos(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[2]
+    yum = yrom + np.sin(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[2]
+    # coords cp lower right foot
+    xr4 = xum + np.sin(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[4]
+    yr4 = yum + np.cos(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[4]
+    # coords of F4
+    xf4 = xr4 + np.sin(np.deg2rad(alp[4] - (alp[2] - (90-c1-alp[0]))))*R[4]
+    yf4 = yr4 - np.cos(np.deg2rad(alp[4] - (alp[2] - (90-c1-alp[0]))))*R[4]
+    # coords cp R3
+    xr3 = xum + np.sin(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[3]
+    yr3 = yum + np.cos(np.deg2rad(alp[2] - (90-c1-alp[0])))*R[3]
+    # coords of F3
+    xf3 = xr3 - np.cos(np.deg2rad(-alp[3] - alp[2] + 180-c1-alp[0]))*R[3]
+    yf3 = yr3 - np.sin(np.deg2rad(-alp[3] - alp[2] + 180-c1-alp[0]))*R[3]
+
+    return [xf1, xom, xf2, xf3, xum, xf4], [yf1, yom, yf2, yf3, yum, yf4]
+
+
+def get_repr(x, r, f):
+    alp, ell, eps = x[0:n_limbs], x[n_limbs:2*n_limbs], x[-1]
+    c1, _, _, _ = _calc_phi(alp, eps)
+    l1, l2, lg, l3, l4 = ell
+    alp1, bet1, gam, alp2, bet2 = alp
+    xf, yf = r
+
+    x, y = [xf[0]], [yf[0]]
+    # draw upper left leg
+    xl1, yl1 = _calc_arc_coords((x[-1], y[-1]), c1, c1+alp1,
+                                _calc_rad(l1, alp1), imgcoords=True)
+    x = x + xl1
+    y = y + yl1
+    # draw torso
+    xt, yt = _calc_arc_coords((x[-1], y[-1]), -90+c1+alp1, -90+c1+alp1+gam,
+                              _calc_rad(lg, gam), imgcoords=True)
+    x = x + xt
+    y = y + yt
+    # draw lower right leg
+    xl4, yl4 = _calc_arc_coords((x[-1], y[-1]), -90+gam-(90-c1-alp1),
+                                -90+gam-(90-c1-alp1)-bet2, _calc_rad(l4, bet2),
+                                imgcoords=True)
+    x = x + xl4
+    y = y + yl4
+    # draw upper right leg
+    xl2, yl2 = _calc_arc_coords((xl1[-1], yl1[-1]), c1+alp1,
+                                c1+alp1+bet1, _calc_rad(l2, bet1),
+                                imgcoords=True)
+    x = x + xl2
+    y = y + yl2
+    # draw lower left leg
+    xl3, yl3 = _calc_arc_coords((xt[-1], yt[-1]), -90+gam-(90-c1-alp1),
+                                -90+gam-(90-c1-alp1)+alp2, _calc_rad(l3, alp2),
+                                imgcoords=True)
+    x = x + xl3
+    y = y + yl3
+
+    fp = ([], [])
+    nfp = ([], [])
+    for idx in range(n_foot):
+        if f[idx]:
+            fp[0].append(xf[idx])
+            fp[1].append(yf[idx])
+        else:
+            nfp[0].append(xf[idx])
+            nfp[1].append(yf[idx])
+
+    return (x, y), fp, nfp
+
+
+def _calc_len(radius, angle):
+    return angle/360.*2*np.pi*radius
+
+
+def _calc_rad(length, angle):
+    return 360.*length/(2*np.pi*angle)
+
+
+def _calc_arc_coords(xy, alp1, alp2, rad, imgcoords=False):
+    x0, y0 = xy
+    x, y = [x0], [y0]
+    xr = x0 + np.cos(np.deg2rad(alp1))*rad
+    if imgcoords:
+        yr = y0 - np.sin(np.deg2rad(alp1))*rad
+    else:
+        yr = y0 + np.sin(np.deg2rad(alp1))*rad
+    steps = [angle for angle in np.linspace(0, alp2-alp1, arc_res)]
+    for dangle in steps:
+        x.append(xr - np.sin(np.deg2rad(90-alp1-dangle))*rad)
+        if imgcoords:
+            y.append(yr + np.cos(np.deg2rad(90-alp1-dangle))*rad)
+        else:
+            y.append(yr - np.cos(np.deg2rad(90-alp1-dangle))*rad)
+
+    return x, y
+
+
+def calc_difference(phi0, phi1):
+    theta = -np.radians(phi0)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
+    x2 = np.cos(np.radians(phi1))
+    y2 = np.sin(np.radians(phi1))
+    vec_ = R*np.c_[[x2, y2, 0]]
+    diff = np.degrees(np.arctan2(float(vec_[1]), float(vec_[0])))
+    return diff
+
+
+def _calc_phi(alpha, eps):
+    c1 = np.mod(eps - alpha[0] - alpha[2]*.5 + 360, 360)
+    c2 = np.mod(c1 + alpha[0] + alpha[1] + 360, 360)
+    c3 = np.mod(180 + alpha[2] - alpha[1] + alpha[3] + c2 + 360, 360)
+    c4 = np.mod(180 + alpha[2] + alpha[0] - alpha[4] + c1 + 360, 360)
+    phi = [c1, c2, c3, c4]
+    return phi

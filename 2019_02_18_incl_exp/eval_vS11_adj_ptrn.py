d8e4a0bad9237b2941d87f1c46ab68d976bc08f2
Lars Schiller (AmP) <lars@familysc.de>
2019-02-21 14:30:07 +0100

    eval incl exp finsih for now

--- 2019_02_18_incl_exp/eval_vS11_adj_ptrn.py
+++ 2019_02_18_incl_exp/eval_vS11_adj_ptrn.py
@@ -19,21 +19,26 @@ from Src import load
 from Src import kin_model
 from Src import predict_pose as pp
 
-col = ev.get_marker_color()
 
-version = 'v40'
+
+version = 'vS11'
 ptrn = 'adj_ptrn'
-incls = ['28', '48', '63', '76']
+incls = ['00', '28', '48', '63', '76_adv']
+#incls = ['00']
 Ts = 0.03
 VOLUME = {'v40': 1,
           'vS11': .7}
+
+
+DEBUG = False
 
 INCL, VELX, VELY, ENERGY = [], [], [], []
 SIGVELX, SIGVELY, SIGENERGY = [], [], []
 
 
 for incl in incls:
-    INCL.append(int(incl))
+    INCL.append(int(incl[:2]))
+
     # %% ### Load Data
 
     dirpath = version+'/'+ptrn+'/incl_'+incl+'/'
@@ -42,6 +47,11 @@ for incl in incls:
     #sets = [sets[0]]
     #print sets
     db, cyc = ev.load_data(dirpath, sets)
+
+    len_cyc = cyc[0][2] - cyc[0][1]
+    prop = 20./len_cyc
+    if incl[:2] == '76' and ptrn == 'adj_ptrn':
+        prop = prop*.5
 
     # correction of epsilon
     rotate = 5
@@ -56,55 +66,58 @@ for incl in incls:
             db[exp]['y{}'.format(marker)] = Y
         db[exp]['eps'] = ev.add_offset(db[exp]['eps'], -eps0+rotate)
 
-
     # %% ### eps during cycle
-    plt.figure('Epsilon corrected')
-    #for exp in range(len(db)):
-    #    for idx in range(6):
-    #        eps = db[exp]['eps'][cyc[exp][1]:cyc[exp][-1]]
-    #        # hack to remove high freq noise
-    #        for idx in range(1, len(eps)):
-    #            if abs(eps[idx] - eps[idx-1]) > 30:
-    #                eps[idx] = eps[idx-1]
-    #        t = db[exp]['time'][cyc[exp][1]:cyc[exp][-1]]
-    #        plt.plot(t, eps, ':', color='mediumpurple')
+    fig, ax = plt.subplots(num='epsilon during cycle '+incl)
+    if DEBUG:
+        for exp in range(len(db)):
+            for idx in range(6):
+                eps = db[exp]['eps'][cyc[exp][1]:cyc[exp][-1]]
+                # hack to remove high freq noise
+                for idx in range(1, len(eps)):
+                    if abs(eps[idx] - eps[idx-1]) > 30:
+                        eps[idx] = eps[idx-1]
+                t = db[exp]['time'][cyc[exp][1]:cyc[exp][-1]]
+                ax.plot(t, eps, ':', color='mediumpurple')
 
     eps, sige = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'eps')
     t, sigt = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'time')
-    eps = ev.downsample(eps)
-    t = ev.downsample(t)
-    sige = ev.downsample(sige)
+    eps = ev.downsample(eps, proportion=prop)
+    t = ev.downsample(t, proportion=prop)
+    sige = ev.downsample(sige, proportion=prop)
 
     t = ev.rm_offset(t)
-    plt.plot(t, eps, '-', color='mediumpurple', linewidth=2)
-    plt.fill_between(t, eps+sige, eps-sige,
-                     facecolor='mediumpurple', alpha=0.5)
-    plt.grid()
-    plt.xlabel(r'time $t$ (s)')
-    plt.ylabel(r'orientation angle $\varepsilon$ ($^\circ$)')
+    TIME = t[-1]
+    ax.plot(t, eps, '-', color='mediumpurple', linewidth=2)
+    ax.fill_between(t, eps+sige, eps-sige,
+                    facecolor='mediumpurple', alpha=0.5)
+    ax.grid()
+    ax.set_xlabel(r'time $t$ (s)')
+    ax.set_ylabel(r'orientation angle $\varepsilon$ ($^\circ$)')
     kwargs = {'extra_axis_parameters': {'x=.1cm', 'y=.1cm'}}
     save.save_as_tikz(dirpath+'eps.tex', **kwargs)
 
-
     # %% ### Track of feet:
-    fig, ax = plt.subplots(subplot_kw=dict(aspect='equal'), num='Track of feet')
-    
-    #for exp in range(len(db)):
-    #    for idx in range(6):
-    #        x = db[exp]['x{}'.format(idx)][cyc[exp][1]:cyc[exp][-1]]
-    #        y = db[exp]['y{}'.format(idx)][cyc[exp][1]:cyc[exp][-1]]
-    #        plt.plot(x, y, color=col[idx])
+    col = ev.get_marker_color()
+    fig, ax = plt.subplots(subplot_kw=dict(aspect='equal'),
+                           num='Track of feet '+incl)
+    prop_track = prop*.5
+
+    if DEBUG:
+        for exp in range(len(db)):
+            for idx in range(6):
+                x = db[exp]['x{}'.format(idx)][cyc[exp][1]:cyc[exp][-1]]
+                y = db[exp]['y{}'.format(idx)][cyc[exp][1]:cyc[exp][-1]]
+                plt.plot(x, y, color=col[idx])
 
     for idx in range(6):
         x, sigx = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'x{}'.format(idx))
         y, sigy = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'y{}'.format(idx))
         if idx == 1:
             DIST = x[-1] - x[0]
-        prop = .05
-        x = ev.downsample(x, prop)
-        y = ev.downsample(y, prop)
-        sigx = ev.downsample(sigx, prop)
-        sigy = ev.downsample(sigy, prop)
+        x = ev.downsample(x, proportion=prop_track)
+        y = ev.downsample(y, proportion=prop_track)
+        sigx = ev.downsample(sigx, proportion=prop_track)
+        sigy = ev.downsample(sigy, proportion=prop_track)
         plt.plot(x, y, color=col[idx])
     #    plt.plot(x[0], y[0], 'o', markersize=20, color=col[idx])
         for xx, yy, sigxx, sigyy in zip(x, y, sigx, sigy):
@@ -120,16 +133,16 @@ for incl in incls:
 
     # %% ### Alpha during cycle:
     col = ev.get_actuator_color()
-    fig, ax = plt.subplots(nrows=2, ncols=1, num='Alpha during cycle', sharex=True)
+    fig, ax = plt.subplots(nrows=2, ncols=1,
+                           num='Alpha during cycle'+incl, sharex=True)
     for axis in range(6):
         alp, siga = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'aIMG{}'.format(axis))
         t, sigt = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'time')
 
         # downsample for tikz
-        prop = .05
-        alp = ev.downsample(alp, prop)
-        t_s = ev.rm_offset(ev.downsample(t, prop))
-        siga = ev.downsample(siga, prop)
+        alp = ev.downsample(alp, proportion=prop)
+        t_s = ev.rm_offset(ev.downsample(t, proportion=prop))
+        siga = ev.downsample(siga, proportion=prop)
 
         if axis in [0, 3, 4]:
             axidx = 0
@@ -155,7 +168,7 @@ for incl in incls:
     db = ev.calc_velocity(db, Ts)
     col = ev.get_actuator_color()
     fig, ax = plt.subplots(nrows=2, ncols=1,
-                           num='velocity during cycle', sharex=True)
+                           num='velocity during cycle'+incl, sharex=True)
     VX, VY = [], []
     SIGVX, SIGVY = [], []
     for axis in [2, 3]:
@@ -172,10 +185,9 @@ for incl in incls:
         SIGVY.append(np.nanstd(vym))
 
         # downsample for tikz
-        prop = .1
-        vx = ev.downsample(vx, prop)
-        vy = ev.downsample(vy, prop)
-        t_s = ev.rm_offset(ev.downsample(t, prop))
+        vx = ev.downsample(vx, proportion=prop)
+        vy = ev.downsample(vy, proportion=prop)
+        t_s = ev.rm_offset(ev.downsample(t, proportion=prop))
         sigvx = ev.downsample(sigvx, prop)
         sigvy = ev.downsample(sigvy, prop)
 
@@ -185,16 +197,18 @@ for incl in incls:
         ax[1].plot(t_s, vy, '-', color=col[axis])
         ax[1].fill_between(t_s, vy+sigvy, vy-sigvy, facecolor=col[axis], alpha=0.5)
 
+    vxmean_ = DIST/TIME
     vxmean = np.mean(VX)
     vymean = np.mean(VY)
     sigvxmean = np.mean(SIGVX)
     sigvymean = np.mean(SIGVY)
-    VELX.append(vxmean)
+    VELX.append(vxmean_)
     VELY.append(vymean)
     SIGVELX.append(sigvxmean)
     SIGVELY.append(sigvymean)
 
     ax[0].plot([t_s[0], t_s[-1]], [vxmean]*2, ':', linewidth=2, color='gray')
+    ax[0].plot([t_s[0], t_s[-1]], [vxmean_]*2, ':', linewidth=2, color='k')
     ax[1].plot([t_s[0], t_s[-1]], [vymean]*2, ':', linewidth=2, color='gray')
 
     ax[0].grid()
@@ -213,7 +227,7 @@ for incl in incls:
     # %% ### pressure during cycle:
     col = ev.get_actuator_color()
     fig, ax = plt.subplots(nrows=2, ncols=1,
-                           num='pressure during cycle', sharex=True)
+                           num='pressure during cycle'+incl, sharex=True)
     MAXPressure = {}
     for axis in range(6):
         p, sigp = ev.calc_mean_of_axis_multi_cyc(db, cyc, 'p{}'.format(axis))
@@ -222,10 +236,9 @@ for incl in incls:
         MAXPressure[axis] = max(r)
 
         # downsample for tikz
-        prop = .05
-        p = ev.downsample(p, prop)
-        t_s = ev.rm_offset(ev.downsample(t, prop))
-        sigp = ev.downsample(sigp, prop)
+        p = ev.downsample(p, proportion=prop)
+        t_s = ev.rm_offset(ev.downsample(t, proportion=prop))
+        sigp = ev.downsample(sigp, proportion=prop)
 
         if axis in [0, 3, 4]:
             axidx = 0
@@ -246,27 +259,37 @@ for incl in incls:
     save.save_as_tikz(dirpath+'pressure.tex', **kwargs)
 
     min_len = min([len(cycle) for cycle in cyc])
-    n_cyc = min_len - 1  # first is skipped
+    n_cyc = min_len - 2  # first is skipped and last too
+    if incl == '76' and ptrn == 'adj_ptrn':
+        n_cyc = n_cyc/2  # ptrn for 76 has twice actuation of each leg
 
-    energy = sum([val[1] for val in MAXPressure.items()])*VOLUME[version]*n_cyc/DIST
+    energy = (sum([val[1]*1e2 for val in MAXPressure.items()])
+              * VOLUME[version] * n_cyc/DIST)
 
-    ENERGY.append(energy)
+    ENERGY.append(abs(energy))
 
 
-# %% VEL via incl
+# %% VEL and ENERGY for incl
 
 VELX = np.array(VELX)
 VELY = np.array(VELY)
 SIGVELX = np.array(SIGVELX)
 SIGVELY = np.array(SIGVELY)
 
-plt.figure('incl- vel')
-plt.plot(INCL, VELX, color='red')
-plt.fill_between(INCL, VELX+SIGVELX, VELX-SIGVELX, facecolor='red', alpha=0.5)
+fig, ax = plt.subplots(num='incl - vel')
+ax.plot(INCL, VELX, color='red')
+ax.fill_between(INCL, VELX+SIGVELX, VELX-SIGVELX, facecolor='red', alpha=0.5)
 
+ax.set_xlabel(r'inclination angle $\delta$ ($^\circ$)')
+ax.set_ylabel(r'mean of velocity $\Delta \bar{x} / \Delta t$ (cm/s)')
+ax.grid()
 
+ax2 = ax.twinx()
+ax2.plot(INCL, ENERGY)
+ax2.set_ylabel(r'Energy Consumption per shift (kJ/cm)')
 
-
+kwargs = {'extra_axis_parameters': {'xtick={0, 28, 48, 63, 76}'}}
+save.save_as_tikz(version+'/incl-vel-energy-{}.tex'.format(ptrn), **kwargs)
 
 
 plt.show()
